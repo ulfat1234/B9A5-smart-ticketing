f6c2fc9a4b80d1c17cbd114e41332b22e93f26a0
ulfat1234 <ulfatsarwar1234@gmail.com>
2024-02-19 03:09:05 +0600

    completed some js functionality

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const allBtn = document.getElementsByClassName('add-btn');
-console.log(allBtn);
 
+// function for scroll to ticket purchase section by clicking Buy Tickets button
+function scrollToTicketSection() {
+    const ticketSection = document.getElementById('ticket-section');
+    ticketSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Access the buttons of bus seats(**accessed only 8 buttons as required**)
+const allBtn = document.getElementsByClassName('add-btn');
+
+
+// Use of utility function for updating numeric values
 let seatLeft = 40;
 let seatSelected = 0;
+let totalPrice = 0;
 for (btn of allBtn) {
     btn.addEventListener('click', function (e) {
         seatLeft = seatLeft - 1;
         setInnerText("seats-left", seatLeft);
-    })
-}
 
-for (btn of allBtn) {
-    btn.addEventListener('click', function (e) {
         seatSelected = seatSelected + 1;
-        setInnerText("seat-selected", seatSelected)
+        setInnerText("seat-selected", seatSelected);
+
+        totalPrice = totalPrice + 550;
+        setInnerText("total-price", totalPrice);
     })
 }
 
-// Utility Function
+// highlight the selected seats
+function setBgColorById(buttonId) {
+    const element = document.getElementById(buttonId);
+    element.classList.remove('bg-[#F7F8F8]');
+    element.classList.add('bg-[#1DD100]');
+}
+
+
+// show popup when click the next button
+const nextBtn = document.getElementById('next');
+nextBtn.addEventListener('click', function (e) {
+    const hiddenItem = document.getElementById('success');
+    hiddenItem.classList.remove('hidden');
+});
+console.log(nextBtn)
+
+// hide popup when click the continue button
+const continueBtn = document.getElementById('continue-btn');
+continueBtn.addEventListener('click', function (e) {
+    const hiddenItem = document.getElementById('success');
+    hiddenItem.classList.add('hidden');
+})
+
+// Utility Function for updating the numeric value of an element
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
